83a56f8fcff0fe41505697f4458a1d4ce76cc328
Sam <sm1215@users.noreply.github.com>
2024-11-07 18:43:51 -0500

    adjust base path once more

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://sm1215.github.io/lights-out/",
+  base: "https://sm1215.github.io/lights-out/dist/",
 });
